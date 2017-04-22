d7cb60e3c281516291fad28603ef29fcc28bc373
Kerrael <mael.querre@gmail.com>
2017-04-23 00:28:23 +0200

    Legères corrections de normes et de nommages

--- partie1a.py
+++ partie1a.py
@@ -1,127 +1,134 @@
 from random import *
 
-#1
-def     creeGrille(n, val):
+
+# 1
+def cree_grille(n, val):
     res = [0] * n
     for i in range(n):
         res[i] = [val] * n
     return res
 
-#2
-def     affiche(grille):
+
+# 2
+def affiche(grille):
     for i in range(len(grille)):
         res = ""
         for j in range(len(grille[i])):
             res += ajout_espace(grille[i][j], 1000) + " "
         print(res)
 
-#Fonction permettant le décalage pour les chiffres inférieur à 1000
-def     ajout_espace(chiffre, quotient):
+
+# Fonction permettant le décalage pour les chiffres inférieurs à 1000
+def ajout_espace(chiffre, quotient):
     res = str(chiffre)
     if chiffre // quotient == 0:
-        if quotient // 10 != 0: 
+        if quotient // 10 != 0:
             res = ajout_espace(chiffre, quotient / 10) + " "
     elif 0 == chiffre < 1000:
         res = str(chiffre) + " "
     return res
 
-#3
-def     appartient(val, grille):
-    for i in range(len(grille)):
-        for j in grille[i]:
-            if j == val:
-                return 1
-    return 0
 
-#3.1
-def     gagnante(grille):
+# 3
+def appartient(val, grille):
+    for x in grille:
+        for y in x:
+            if y == val:
+                return True
+    return False
+
+
+# 3.1
+def gagnante(grille):
     return appartient(2048, grille)
 
-#3.2
-def     pleine(grille):
-    if appartient(0, grille) == 1:
-        return 0
-    return 1
 
-#4
-def     max(grille):
-    maxi = 0
-    
-    for i in range(len(grille)):
-        for j in range(len(grille[i])):
-            if grille[i][j] >= maxi:
-                maxi = grille[i][j]
-    return maxi
+# 3.2
+def pleine(grille):
+    return not appartient(0, grille)
 
-#5.a
-def     lescases(val, grille):
+
+# 4
+def valeur_max(grille):
+    res = 0
+    for x in grille:
+        for y in x:
+            if y > res:
+                res = y
+    return res
+
+
+# 5.a
+def les_cases(grille, val):
     liste = []
-
     for i in range(len(grille)):
-        for j in range(len(grille[i])):
-            newliste = []
-            if val == grille[i][j]:
-                newliste.extend((i,j))
-                liste.append(newliste)
+        for j in range(len(grille)):
+            if grille[i][j] == val:
+                liste.append([i, j])
     return liste
 
-#5.b
-def     vides(grille):
-     return lescases(0, grille)
 
-#5.c
-def     ajouteAlea(val, grille):
-    nbr = randint(0, len(vides(grille)) - 1)
-    pos = vides(grille)
-    grille[pos[nbr][0]][pos[nbr][1]] = val
+# 5.b
+def vides(grille):
+    return les_cases(grille, 0)
+
+
+# 5.c
+def ajoute_alea(grille, val):
+    pos = choice(vides(grille))
+    grille[pos[0]][pos[1]] = val
     return grille
 
-#5.d
-def     init(n):
-    grille = creeGrille(n, 0)
-    nbr1 = 2 ** randint(1, 2)
-    nbr2 = 2 ** randint(1, 2)
-    grille = ajouteAlea(nbr1, grille)
-    grille = ajouteAlea(nbr2, grille)
+
+# 5.d
+def init(n):
+    grille = cree_grille(n, 0)
+    val1, val2 = randrange(2, 5, 2), randrange(2, 5, 2)
+    grille = ajoute_alea(grille, val1)
+    grille = ajoute_alea(grille, val2)
     return grille
 
-#6
 
-#Fonction permettant de retirer tou les zéro d'une chaine
-def     coupezero(liste):
+# 6
+
+# Fonction permettant de retirer tou les zéro d'une chaine
+def coupe_zero(liste):
     newliste = []
-    for i in range(len(liste)):
-        if liste[i] != 0:
-            newliste.append(liste[i])
+    for x in liste:
+        if x != 0:
+            newliste.append(x)
     return newliste
 
-def     fusion(liste, i, j):
+
+def fusion(liste, i, j):
     liste[i] = liste[j] * 2
     liste[j] = 0
     return liste
 
-def     haut(grille):
+
+def haut(grille):
     for i in range(len(grille)):
         lst = []
         for j in range(len(grille[i])):
-            total = 0
             lst.append(grille[j][i])
         x = 0
+        total = 0
         while x < len(lst):
             if lst[x] != total:
                 total = lst[x]
             elif lst[x] != 0:
                 total = 0
-                lst = fusion(lst, x, x-1)
-            x += 1        
-        ligne = coupezero(lst)
+                lst = fusion(lst, x, x - 1)
+            x += 1
+        ligne = coupe_zero(lst)
         size = len(ligne)
         for y in range(4 - size):
-                ligne.append(0)
+            ligne.append(0)
         for y in range(len(grille)):
             grille[y][i] = ligne[y]
 
-def     bas(grille):
+
+def bas(grille):
     for i in range(len(grille)):
         lst = []
         for j in range(len(grille[i])):
@@ -133,16 +140,17 @@ def     bas(grille):
                 total = lst[x]
             elif lst[x] != 0:
                 total = 0
-                lst = fusion(lst, x, x+1)
-            x -= 1       
-        ligne = coupezero(lst)
+                lst = fusion(lst, x, x + 1)
+            x -= 1
+        ligne = coupe_zero(lst)
         size = len(ligne)
         for y in range(4 - size):
             ligne.insert(y, 0)
         for y in range(len(grille[i])):
             grille[y][i] = ligne[y]
 
-def     droite(grille):
+
+def droite(grille):
     for i in range(len(grille)):
         lst = []
         for j in range(len(grille[i])):
@@ -154,16 +162,17 @@ def     droite(grille):
                 total = lst[x]
             elif lst[x] != 0:
                 total = 0
-                lst = fusion(lst, x, x+1)
-            x -= 1        
-        ligne = coupezero(lst)
-        size =len(ligne)
+                lst = fusion(lst, x, x + 1)
+            x -= 1
+        ligne = coupe_zero(lst)
+        size = len(ligne)
         for y in range(4 - size):
             ligne.insert(y, 0)
-        for y in range(len(grille[i])): 
+        for y in range(len(grille[i])):
             grille[i][y] = ligne[y]
 
-def     gauche(grille):
+
+def gauche(grille):
     for i in range(len(grille)):
         lst = []
         for j in range(len(grille[i])):
@@ -175,22 +184,22 @@ def     gauche(grille):
                 total = lst[x]
             elif lst[x] != 0:
                 total = 0
-                lst = fusion(lst, x, x-1)
+                lst = fusion(lst, x, x - 1)
             x += 1
-        ligne = coupezero(lst)
-        size =len(ligne)
+        ligne = coupe_zero(lst)
+        size = len(ligne)
         for y in range(4 - size):
             ligne.append(0)
-        for y in range(len(grille[i])): 
+        for y in range(len(grille[i])):
             grille[i][y] = ligne[y]
 
-#7
-def     partie(n):
+
+# 7
+def partie(n):
     grille = init(n)
     affiche(grille)
     stop = 0
     limite = 0
-
     while stop != 1:
         coup = input("action ?('h', 'b', 'd', 'g')")
         if coup == "h" and limite != 2:
@@ -201,42 +210,42 @@ def     partie(n):
             droite(grille)
         elif coup == "g" and limite != 1:
             gauche(grille)
-        
         stop = gagnante(grille)
         if stop == 1:
-            print("vous avez gagné")
+            print("Vous avez gagné")
         nbr1 = 2 ** randint(1, 2)
         nbr2 = 2 ** randint(1, 2)
-        print(nbr1, nbr2)
-        grille = ajouteAlea(nbr1, grille)
-        grille = ajouteAlea(nbr2, grille)
+        grille = ajoute_alea(grille, nbr1)
+        grille = ajoute_alea(grille, nbr2)
         affiche(grille)
         if pleine(grille) == 1:
-#            liste = lescases(2, grille)
-#            limite = perdante(liste)
-#            if limite == 0:
-            print("vous avez perdu, votre score est de : ", max(grille))
+            #            liste = les_cases(2, grille)
+            #            limite = perdante(liste)
+            #            if limite == 0:
+            print("Vous avez perdu, votre score est de : ", valeur_max(grille))
             stop = 1
 
-#fonction permettant de détection si il reste un coup jouable
-def     perdante(liste):
+
+# fonction permettant de détection si il reste un coup jouable
+def perdante(liste):
     compt = -1
     total = -1
     for i in range(len(liste)):
         if liste[i][0] != compt:
             compt = liste[i][0]
         elif compt != -1:
-            #print(i, (liste[i-1][1]+1), liste[i][1])
-            if (liste[i-1][1]+1) == liste[i][1]:
+            # print(i, (liste[i-1][1]+1), liste[i][1])
+            if (liste[i - 1][1] + 1) == liste[i][1]:
                 return 1
         if liste[i][1] != total:
             total = liste[i][1]
         elif total != -1:
-            #print("e1", (liste[i-1][0]+1), liste[i][0])
-            if (liste[i][0] ) == liste[i][0]:
+            # print("e1", (liste[i-1][0]+1), liste[i][0])
+            if (liste[i][0]) == liste[i][0]:
                 return 2
     return 0
 
-g1=[[0, 0, 2, 2],[4, 2, 2, 8],[0, 2, 32, 0],[128, 2, 1024, 2048]]
-g2=[[4, 2, 16, 8], [8, 2, 4, 2], [2, 2, 2, 4], [4, 4, 4, 2]]
+
+g1 = [[0, 0, 2, 2], [4, 2, 2, 8], [0, 2, 32, 0], [128, 2, 1024, 2048]]
+g2 = [[4, 2, 16, 8], [8, 2, 4, 2], [2, 2, 2, 4], [4, 4, 4, 2]]
 partie(4)
--- partie1b.py
+++ partie1b.py
@@ -1,209 +1,229 @@
 from random import *
 from tkinter import *
 
-n = 4
+nbr = 4
 grille = []
 
-#1
-def     creeGrille(n, val):
+
+# 1
+def cree_grille(n, val):
     res = [0] * n
     for i in range(n):
         res[i] = [val] * n
     return res
 
-#2
-def     affiche(grille):
-    for i in range(len(grille)):
+
+# 2
+def affiche_grille(g):
+    for i in range(len(g)):
         res = ""
-        for j in range(len(grille[i])):
-            res += ajout_espace(grille[i][j], 1000) + " "
+        for j in range(len(g[i])):
+            res += ajout_espace(g[i][j], 1000) + " "
         print(res)
 
-#Fonction permettant le décalage pour les chiffres inférieur à 1000
-def     ajout_espace(chiffre, quotient):
+
+# Fonction permettant le décalage pour les chiffres inférieur à 1000
+def ajout_espace(chiffre, quotient):
     res = str(chiffre)
     if chiffre // quotient == 0:
-        if quotient // 10 != 0: 
+        if quotient // 10 != 0:
             res = ajout_espace(chiffre, quotient / 10) + " "
     elif 0 == chiffre < 1000:
         res = str(chiffre) + " "
     return res
 
-#3
-def     appartient(val, grille):
-    for i in range(len(grille)):
-        for j in grille[i]:
-            if j == val:
-                return 1
-    return 0
 
-#3.1
-def     gagnante(grille):
-    return appartient(2048, grille)
+# 3
+def appartient(val, g):
+    for x in g:
+        for y in x:
+            if y == val:
+                return True
+    return False
 
-#3.2
-def     pleine(grille):
-    if appartient(0, grille) == 1:
-        return 0
-    return 1
 
-#4
-def     max(grille):
+# 3.1
+def gagnante(g):
+    return appartient(2048, g)
+
+
+# 3.2
+def pleine(g):
+    return not appartient(0, g)
+
+
+# 4
+def valeur_max(g):
     maxi = 0
-    
-    for i in range(len(grille)):
-        for j in range(len(grille[i])):
-            if grille[i][j] >= maxi:
-                maxi = grille[i][j]
+
+    for i in range(len(g)):
+        for j in range(len(g[i])):
+            if g[i][j] >= maxi:
+                maxi = g[i][j]
     return maxi
 
-#5.a
-def     lescases(val, grille):
+
+# 5.a
+def les_cases(g, val):
     liste = []
 
-    for i in range(len(grille)):
-        for j in range(len(grille[i])):
+    for i in range(len(g)):
+        for j in range(len(g[i])):
             newliste = []
-            if val == grille[i][j]:
-                newliste.extend((i,j))
+            if val == g[i][j]:
+                newliste.extend((i, j))
                 liste.append(newliste)
     return liste
 
-#5.b
-def     vides(grille):
-     return lescases(0, grille)
 
-#5.c
-def     ajouteAlea(val, grille):
-    nbr = randint(0, len(vides(grille)) - 1)
-    pos = vides(grille)
-    grille[pos[nbr][0]][pos[nbr][1]] = val
-    return grille
+# 5.b
+def vides(g):
+    return les_cases(g, 0)
 
-#5.d
-def     init(n):
-    grille = creeGrille(n, 0)
+
+# 5.c
+def ajoute_alea(g, val):
+    pos = choice(vides(g))
+    g[pos[0]][pos[1]] = val
+    return g
+
+
+# 5.d
+def init(n):
+    global grille
+    grille = cree_grille(n, 0)
     nbr1 = 2 ** randint(1, 2)
     nbr2 = 2 ** randint(1, 2)
-    grille = ajouteAlea(nbr1, grille)
-    grille = ajouteAlea(nbr2, grille)
+    grille = ajoute_alea(grille, nbr1)
+    grille = ajoute_alea(grille, nbr2)
     return grille
 
-#6
 
-#Fonction permettant de retirer tou les zéro d'une chaine
-def     coupezero(liste):
+# 6
+
+# Fonction permettant de retirer tou les zéro d'une chaine
+def coupezero(liste):
     newliste = []
     for i in range(len(liste)):
         if liste[i] != 0:
             newliste.append(liste[i])
     return newliste
 
-def     fusion(liste, i, j):
+
+def fusion(liste, i, j):
     liste[i] = liste[j] * 2
     liste[j] = 0
     return liste
 
-def     haut(grille):
-    global n
-    for i in range(len(grille)):
+
+def haut(g):
+    global nbr
+    total = 0
+    for i in range(len(g)):
         lst = []
-        for j in range(len(grille[i])):
+        for j in range(len(g[i])):
             total = 0
-            lst.append(grille[j][i])
+            lst.append(g[j][i])
         x = 0
         while x < len(lst):
             if lst[x] != total:
                 total = lst[x]
             elif lst[x] != 0:
                 total = 0
-                lst = fusion(lst, x, x-1)
-            x += 1        
-        ligne = coupezero(lst)
-        size = len(ligne)
-        for y in range(n - size):
-                ligne.append(0)
-        for y in range(len(grille)):
-            grille[y][i] = ligne[y]
-    return grille
-
-def     bas(grille):
-    global n
-    for i in range(len(grille)):
-        lst = []
-        for j in range(len(grille[i])):
-            total = 0
-            lst.append(grille[j][i])
-        x = len(lst) - 1
-        while x >= 0:
-            if lst[x] != total:
-                total = lst[x]
-            elif lst[x] != 0:
-                total = 0
-                lst = fusion(lst, x, x+1)
-            x -= 1       
-        ligne = coupezero(lst)
-        size = len(ligne)
-        for y in range(n - size):
-            ligne.insert(y, 0)
-        for y in range(len(grille[i])):
-            grille[y][i] = ligne[y]
-
-    return grille
-
-def     droite(grille):
-    global n
-    for i in range(len(grille)):
-        lst = []
-        for j in range(len(grille[i])):
-            lst.append(grille[i][j])
-        total = 0
-        x = len(lst) - 1
-        while x >= 0:
-            if lst[x] != total:
-                total = lst[x]
-            elif lst[x] != 0:
-                total = 0
-                lst = fusion(lst, x, x+1)
-            x -= 1        
-        ligne = coupezero(lst)
-        size =len(ligne)
-        for y in range(n - size):
-            ligne.insert(y, 0)
-        for y in range(len(grille[i])): 
-            grille[i][y] = ligne[y]
-    return grille
-
-def     gauche(grille):
-    global n
-    for i in range(len(grille)):
-        lst = []
-        for j in range(len(grille[i])):
-            lst.append(grille[i][j])
-        total = 0
-        x = 0
-        while x < len(lst):
-            if lst[x] != total:
-                total = lst[x]
-            elif lst[x] != 0:
-                total = 0
-                lst = fusion(lst, x, x-1)
+                lst = fusion(lst, x, x - 1)
             x += 1
         ligne = coupezero(lst)
-        size =len(ligne)
-        for y in range(n - size):
+        size = len(ligne)
+        for y in range(nbr - size):
             ligne.append(0)
-        for y in range(len(grille[i])): 
-            grille[i][y] = ligne[y]
+        for y in range(len(g)):
+            g[y][i] = ligne[y]
+    return g
 
-    return grille
-#7
-def     partie(n):
+
+def bas(g):
+    global nbr
+    total = 0
+    for i in range(len(g)):
+        lst = []
+        for j in range(len(g[i])):
+            total = 0
+            lst.append(g[j][i])
+        x = len(lst) - 1
+        while x >= 0:
+            if lst[x] != total:
+                total = lst[x]
+            elif lst[x] != 0:
+                total = 0
+                lst = fusion(lst, x, x + 1)
+            x -= 1
+        ligne = coupezero(lst)
+        size = len(ligne)
+        for y in range(nbr - size):
+            ligne.insert(y, 0)
+        for y in range(len(g[i])):
+            g[y][i] = ligne[y]
+
+    return g
+
+
+def droite(g):
+    global nbr
+    total = 0
+    for i in range(len(g)):
+        lst = []
+        for j in range(len(g[i])):
+            total = 0
+            lst.append(g[i][j])
+        x = len(lst) - 1
+        while x >= 0:
+            if lst[x] != total:
+                total = lst[x]
+            elif lst[x] != 0:
+                total = 0
+                lst = fusion(lst, x, x + 1)
+            x -= 1
+        ligne = coupezero(lst)
+        size = len(ligne)
+        for y in range(nbr - size):
+            ligne.insert(y, 0)
+        for y in range(len(g[i])):
+            g[i][y] = ligne[y]
+    return g
+
+
+def gauche(g):
+    global nbr
+    for i in range(len(g)):
+        lst = []
+        for j in range(len(g[i])):
+            lst.append(g[i][j])
+        total = 0
+        x = 0
+        while x < len(lst):
+            if lst[x] != total:
+                total = lst[x]
+            elif lst[x] != 0:
+                total = 0
+                lst = fusion(lst, x, x - 1)
+            x += 1
+        ligne = coupezero(lst)
+        size = len(ligne)
+        for y in range(nbr - size):
+            ligne.append(0)
+        for y in range(len(g[i])):
+            g[i][y] = ligne[y]
+
+    return g
+
+
+# 7
+def partie(n):
+    global grille
     grille = init(n)
-    affiche(grille)
+    affiche_grille(grille)
     stop = 0
     limite = 0
-
     while stop != 1:
         coup = input("action ?('h', 'b', 'd', 'g')")
         if coup == "h" and limite != 2:
@@ -214,72 +234,80 @@ def     partie(n):
             droite(grille)
         elif coup == "g" and limite != 1:
             gauche(grille)
-        
         stop = gagnante(grille)
         if stop == 1:
             print("vous avez gagné")
         nbr1 = 2 ** randint(1, 2)
         nbr2 = 2 ** randint(1, 2)
         print(nbr1, nbr2)
-        grille = ajouteAlea(nbr1, grille)
-        grille = ajouteAlea(nbr2, grille)
-        affiche(grille)
+        grille = ajoute_alea(nbr1, grille)
+        grille = ajoute_alea(nbr2, grille)
+        affiche_grille(grille)
         if pleine(grille) == 1:
-#            liste = lescases(2, grille)
-#            limite = perdante(liste)
-#            if limite == 0:
-            print("vous avez perdu, votre score est de : ", max(grille))
+            # liste = les_cases(grille, 2)
+            # limite = perdante(liste)
+            # if limite == 0:
+            print("vous avez perdu, votre score est de : ", valeur_max(grille))
             stop = 1
 
-#fonction permettant de détection si il reste un coup jouable
-def     perdante(liste):
+
+# fonction permettant de détection si il reste un coup jouable
+def perdante(liste):
     compt = -1
     total = -1
     for i in range(len(liste)):
         if liste[i][0] != compt:
             compt = liste[i][0]
         elif compt != -1:
-            #print(i, (liste[i-1][1]+1), liste[i][1])
-            if (liste[i-1][1]+1) == liste[i][1]:
+            # print(i, (liste[i-1][1]+1), liste[i][1])
+            if (liste[i - 1][1] + 1) == liste[i][1]:
                 return 1
         if liste[i][1] != total:
             total = liste[i][1]
         elif total != -1:
-            #print("e1", (liste[i-1][0]+1), liste[i][0])
-            if (liste[i][0] ) == liste[i][0]:
+            # print("e1", (liste[i-1][0]+1), liste[i][0])
+            if (liste[i][0]) == liste[i][0]:
                 return 2
     return 0
 
-#Q2
-def     message(chaine):
+
+# Q2
+def message(chaine):
     zone_texte.delete("0.0", END)
     zone_texte.insert(END, chaine)
 
-#Q3
-def     dessineGrille(grille):
+
+# Q3
+def dessine_grille(g):
     c = 40
     x0 = 100
     y0 = 100
-    for i in range(len(grille)):
-        for j in range(len(grille[i])):
-            x = grille[i][j]
+    n = len(g)
+    for i in range(n):
+        for j in range(n):
+            x = g[i][j]
             color = str(couleur(x))
-            case = Frame(fram, bg=color, width=100, height=100)
-            case.grid(row=i, column=j, padx=1, pady=1)
+            case = Frame(fond, width=100, height=100)
+            case.grid(row=i, column=j, padx=2, pady=2)
             if x != 0:
-                text = Label(case, text=x, bg=color, width=4, height=2, justify=CENTER)
+                text = Label(case, text=x, width=8, height=4, bg=color,
+                             font=("Ubuntu", 20, "bold"), justify=CENTER)
             else:
-                text = Label(case, text="", bg=color, width=4, height=2, justify=CENTER)    
+                text = Label(case, text="", width=8, height=4, bg=color,
+                             font=("Ubuntu", 20, "bold"), justify=CENTER)
             text.grid()
 
-#Q3.1
-def     couleur(nbr):
-    dico={0:"#FFFFFF", 2:"#91FFD2", 4:"#B2D8E7", 8:"#B2C4DF", 16:"#6FB8FF", 32:"#339ED7", 64:"#50EEEF", 128:"#6893EC", 256:"#0040EC", 512:"#6F40E0", 1024:"#7C0C59", 2048:"#C50CD1"}
 
-    return dico[nbr]
+# Q3.1
+def couleur(n):
+    dico = {0: "#FFFFFF", 2: "#91FFD2", 4: "#B2D8E7", 8: "#B2C4DF",
+            16: "#6FB8FF", 32: "#339ED7", 64: "#50EEEF", 128: "#6893EC",
+            256: "#0040EC", 512: "#6F40E0", 1024: "#7C0C59", 2048: "#C50CD1"}
+    return dico[n]
 
-#Q4
-def     clavier(event):
+
+# Q4
+def clavier(event):
     global grille
     event = event.keysym
     if event == "Up":
@@ -290,41 +318,51 @@ def     clavier(event):
         grille = gauche(grille)
     elif event == "Right":
         grille = droite(grille)
-
     nbr1 = 2 ** randint(1, 2)
     nbr2 = 2 ** randint(1, 2)
-    grille = ajouteAlea(nbr1, grille)
-    grille = ajouteAlea(nbr2, grille)
-    dessineGrille(grille)
+    grille = ajoute_alea(grille, nbr1)
+    grille = ajoute_alea(grille, nbr2)
+    dessine_grille(grille)
     if gagnante(grille) == 1:
-        message("Bravo vous avec gagné, vous pouvez recommencé")
+        message("Bravo vous avec gagné, vous pouvez recommencer")
     elif pleine(grille) == 1:
-        msg = "Perdus votre score est de"
-        msg.append(max(grille))
+        msg = "Perdu ! Votre score est de " + str(valeur_max(grille)) + ", vous pouvez recommencer"
+        msg += (valeur_max(grille))
         message(msg)
- 
 
-def     fermer():
+
+def fermer():
     plateau.quit()
     plateau.destroy()
 
-def     jeu():
+
+def jeu():
+    global nbr
     global grille
-    grille = init(n)
-    dessineGrille(grille)
+    grille = init(nbr)
+    dessine_grille(grille)
 
-#Q1
+
+# Q1
 plateau = Tk()
-plateau.title("jeu 2048 -version L1 2017")
+plateau.title("2048 - Version L1 2017")
 
-fram = Frame(plateau, bg="grey", width=500, height=500)
-fram.grid(row=1, column=0)
-bdep = Button(plateau, text="jouer", command=jeu, justify=CENTER,font=("Ubuntu", 20, "bold"))
-bdep.grid(row=0, column=1)
-zone_texte = Text(plateau, width = 40, height = 5)
+# Cadre blanc pour pouvoir y appliquer une marge
+cadre = Frame(plateau, padx=20, pady=20, bg="blue")
+fond = Frame(cadre, bg="gray", padx=2, pady=2,
+             width=300, height=300)
+fond.grid()
+cadre.grid(row=0, rowspan=3)
+
+bJouer = Button(plateau, text="Jouer", command=jeu,
+                justify=CENTER, font=("Ubuntu", 20))
+bJouer.grid(row=0, column=1)
+
+zone_texte = Text(plateau, width=40, height=5)
 zone_texte.grid(row=1, column=1)
-bqui = Button(plateau, text="Quitter", command=fermer, font=("Ubuntu", 20, "bold"))
-bqui.grid(row=2, column=1)
+bQuitter = Button(plateau, text="Quitter", command=fermer,
+                  font=("Ubuntu", 20))
+bQuitter.grid(row=2, column=1)
 
 plateau.bind("<Up>", clavier)
 plateau.bind("<Down>", clavier)
